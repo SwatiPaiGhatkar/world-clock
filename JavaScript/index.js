fd95f1537bc9d9498623014d06e9e4f840fde0aa
SwatiPaiGhatkar <swatipaighatkar@gmail.com>
2025-06-23 19:01:06 +1000

    added ID to select drop down, then added TimeZone values to the options, added functionality for the select.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -19,3 +19,25 @@ setInterval(function () {
     "h:mm:ss [<small>]A[</small>]"
   );
 }, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+
+  let cityElement = document.querySelector("#container-city");
+  cityElement.innerHTML = `
+  <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date"> ${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small> ${cityTime.format("A")}</small></div>
+      </div>
+  
+  `;
+}
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
